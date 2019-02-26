45b19d8a2b57b099b2796eddb85fd778bc7114ce
huynhvivian <47425937+huynhvivian@users.noreply.github.com>
2019-02-25 21:14:32 -0700

    Add files via upload

--- Board.java
+++ Board.java
@@ -2,13 +2,17 @@ import java.util.*;
 
 public class Board {
 
+  // Instance variables
   private int[][] board;
-
-  // variables for checking moves //
-  private int n; //
+  private int n; // counts how many pieces are flipped in a direction
   private int[] flipped = new int[9];
   private int otherPlayer = 0;
 
+  /**
+   * This is the default constructor and it creates a 2D array which represents
+   * the board around the board it creates a border with 3's this is to checking
+   * moves at the edges of the board
+   */
   public Board() {
     this.board = new int[10][10];
     for (int i = 1; i < this.board.length; i++) {
@@ -17,7 +21,7 @@ public class Board {
       }
     }
 
-    // Fills edges of board with "#"'s to help with edge cases
+    // Fills edges of board with 3's to help with edge cases
     for (int j = 0; j < 10; j++) {
       for (int i = 0; i < 10; i++) {
         board[0][j] = 3; // top
@@ -26,22 +30,26 @@ public class Board {
         board[i][9] = 3; // right
       }
     }
-    /**this sets pieces in the middle of the board to set up the initial board*/
+    /** this sets pieces in the middle of the board to set up the initial board */
     board[4][4] = 1;
     board[4][5] = 2;
     board[5][4] = 2;
     board[5][5] = 1;
   }
 
+  /**
+   * prints the board on to the terminal so that the user can visualize it prints
+   * labels on the x and y axis so that it is easier for the user to see it
+   */
   public void printBoard() {
-    System.out.println("    1 2 3 4 5 6 7 8  ");
-    for (int i = 0; i <= 9; i++) {
+    System.out.println("   1 2 3 4 5 6 7 8  ");
+    for (int i = 1; i <= 8; i++) {
       if ((i < 1) || (i > 8)) {
-        System.out.print(" |");
+        System.out.print("  |"); // leaves out the 0 and 9 on the left column of numbers
       } else {
-        System.out.print(i + "|");
+        System.out.print(i + " |"); // prints out number column on left side
       }
-      for (int j = 0; j <= 9; j++) {
+      for (int j = 1; j <= 8; j++) {
         System.out.print(board[i][j] + "|");
       }
       System.out.print(" " + "\n");
@@ -49,8 +57,12 @@ public class Board {
   }
 
   /**
-   * takes in user input @param x, @param y, and then checks for valid moves, if
-   * the move is valid, the board updates accordingly
+   * takes in user input
+   * 
+   * @param x      = x coordinate that the user inputs
+   * @param y      = y coordinate the user inputs
+   * @param player = which player's turn it is
+   * @param array  =
    */
   public void updateBoard(int x, int y, int player, int[] array) {
     int north = array[1];
@@ -110,6 +122,11 @@ public class Board {
     }
   }
 
+  /**
+   * calculates the score of the player whose move it is takes in
+   * 
+   * @param player and @return turnScore
+   */
   public int turnScore(int player) {
     int turnScore = 0;
     if (player == 1) {
@@ -130,6 +147,11 @@ public class Board {
     return turnScore;
   }
 
+  /**
+   * checks the state of the board and chcecks whether it is full
+   * 
+   * @return true if the board is full and ends the game
+   */
   public boolean gameOver() {
     boolean full = false;
     int countTotal = 0;
@@ -152,23 +174,26 @@ public class Board {
   /**
    * @param x = x coordinate of point to check
    * @param y = y coordinate of point to check
-   * @return flipped returns a nine integer array, first element is total number
-   *         of pieces that would be flipped, the second is the number of pieces
+   * @return flipped = a nine integer array, first element is total number of
+   *         pieces that would be flipped, the second is the number of pieces
    *         flipped in the north direction, third is pieces flipped northeast,
    *         continuing clockwise for all 8 directions.
    **/
   public int[] move(int x, int y, int player) {
 
+    // resets the flipped array everytime the player tries to make a move
     for (int e = 0; e < 9; e++) {
       flipped[e] = 0;
     }
 
+    // determines the number of the other player
     if (player == 1) {
       otherPlayer = 2;
     } else if (player == 2) {
       otherPlayer = 1;
     }
 
+    // checks if spot is empty, otherwise does nothing //
     if (board[y][x] == 0) {
       n = 1;
 
@@ -257,7 +282,7 @@ public class Board {
         while (board[y + n][x - n] == otherPlayer) {
           n += 1;
         }
-        if (board[y + n][x - n] == otherPlayer) {
+        if (board[y + n][x - n] == 3) {
           n = 1;
         } else if (board[y + n][x - n] == 0) {
           n = 1;
--- Othello.java
+++ Othello.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
 public class Othello {
+private static Scanner coord = new Scanner(System.in);
 
     public static void main(String[] args) {
         Board board = new Board(); // creates a new Board object
         int player = 0;// sets player to 0, this means there are no players at this time
         Player player_1 = new Player();
         Player player_2 = new Player();
-        Scanner coord = new Scanner(System.in);
 
         /**
          * Use math.random to decide which players turn it is if the number generated is
@@ -27,7 +27,10 @@ public class Othello {
         int playerOneScore = player_1.getScore();
         int playerTwoScore = player_2.getScore();
 
-        /** gameOver checks whether board is full of pieces */
+        /** gameOver checks whether board is full of pieces
+         * if the board is full the game ends and the winner's are announced
+         * otherwise the game keeps going
+         */
         while (board.gameOver() == false) {
             if (player == 1) {
                 board.printBoard();
@@ -46,6 +49,7 @@ public class Othello {
                     x = coord.nextInt();
                     System.out.print("Enter a number between 1 to 8 for the x coordinate: ");
                     y = coord.nextInt();
+                    flipped = board.move(x, y, player);
                 }
                 if (flipped[0] != 0) {
                     board.updateBoard(x, y, player, flipped);
@@ -63,18 +67,19 @@ public class Othello {
                 int x = coord.nextInt();
                 System.out.print("Enter a number between 1 to 8 for the y coordinate: ");
                 int y = coord.nextInt();
+                
+                int[] flipped = board.move(x, y, player);
 
-                while ((x < 1 || x > 8) || (y < 1 || y > 8)) {
+                while ((x < 1 || x > 8) || (y < 1 || y > 8) || (flipped[0] == 0)) {
                     player = 2;
                     System.out.println("Invalid move, try again");
                     System.out.print("Enter a number between 1 to 8 for the x coordinate: ");
                     x = coord.nextInt();
                     System.out.print("Enter a number between 1 to 8 for the y coordinate: ");
                     y = coord.nextInt();
+                    flipped = board.move(x, y, player);
                 }
 
-                int[] flipped = board.move(x, y, player);
-                System.out.print("Flipped" + Arrays.toString(flipped));
 
                 if (flipped[0] != 0) {
                     board.updateBoard(x, y, player, flipped);
@@ -85,5 +90,18 @@ public class Othello {
                 player = 1;
             }
         }
+        if (board.gameOver() == true){
+            board.printBoard();
+            if (player_1.getScore() > player_2.getScore()){
+                System.out.println("Player 1 wins! Final score is: " + player_1.getScore());
+            }
+            else if (player_2.getScore() < player_2.getScore()){
+                System.out.println("Player 2 wins! Final score is: " + player_2.getScore());
+            }
+            else if(player_2.getScore() == player_2.getScore()){
+                System.out.println("Its a draw! " + "Player 1's score: " + player_1.getScore() + "Player 2's score: "+ player_2.getScore());
+            }
+        }
     }
-}
+
+}        
