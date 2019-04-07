0763d9775f0982596f7bc0e82c155871c3f384aa
velvetarchangel <32275484+velvetarchangel@users.noreply.github.com>
2019-04-07 13:53:40 -0600

    Update Othello.java

--- Othello.java
+++ Othello.java
@@ -1,6 +1,12 @@
 import java.util.*;
 import java.util.Arrays;
 
+
+
+/**Version 1.0 
+* @author Himika Dastidar
+* this class runs the text based version of the game using other classes*/
+
 public class Othello{
 
     public static void main(String[] args){
@@ -9,12 +15,13 @@ public class Othello{
         Player[] players = new Player[2]; // creates a player array and initializes it
         OthelloHelper game = new OthelloHelper();
             game.greeting(); // Prints welcome message and instructions using method
-            String choice = kb.nextLine();
+            String choice = kb.nextLine(); // allows the user to select the game mode
             switch(choice){
-                case "q":
+                case "q": // quits at any stage of the game
                     game.finishGame(players[0], players[1],board);
                     break;
-                case "p":
+                
+                case "p": // initializes player vs player mode
                     System.out.println("Please enter a name:");
                     String name1 = kb.nextLine();
                     System.out.println("Please enter another name:");
@@ -24,11 +31,12 @@ public class Othello{
                     System.out.println(players[0].getName() + " is Player 1.");
                     game.Player_v_Player(board, players);
                     break;
-                case "c":
+                
+                case "c": // initializes the player vs AI mode of the game
                     System.out.println("Playing against computer.");
                     System.out.println("Please enter your name:");
                     String name3 = kb.nextLine();
-                    String name4 = "Karel"; // not needed
+                    String name4 = "Karel"; 
                     players = game.initial_turn(name3, name4, "computer"); // Decides which player goes first
                     System.out.println(players[0].getName() + " is Player 1.");
                     game.Player_v_AI(board, players);
